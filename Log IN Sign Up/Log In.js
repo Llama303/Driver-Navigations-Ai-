a873c31d0468c505e32bddd86b2ad027440918b9
Meko:) <burghermek@gmail.com>
2026-03-10 13:36:34 -0700

    More login page improvments

---
+++ Log IN Sign Up/Log In.js
@@ -0,0 +1,10 @@
+username = document.getElementById('username');
+if(username.length == 0){
+    username.style.border = '1px solid red';
+    username.placeholder = 'Please enter a username';
+}
+password = document.getElementById('password');
+if(password.length == 0){
+    password.style.border = '1px solid red';
+    password.placeholder = 'Please enter a password';
+}
